9b19587db99b7efdc440fd5e5979f5fe662c5021
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-01 15:39:23 +0100

    Scripts

--- .github/scripts/Bindings/SDL.sh
+++ .github/scripts/Bindings/SDL.sh
@@ -1,79 +1,107 @@
 #!/usr/bin/env bash
 set +e
 
-BASEPATH="$(cd "$WORKSPACE" && pwd)"
-
-git clone --recurse-submodules https://github.com/libsdl-org/SDL.git "$BASEPATH/SDL"
-
-OUTPUTS="$BASEPATH/outputs"
+# Setup
+BASEPATH="$(pwd)"
+OUTPUTS="$BASEPATH/Bindings/Output"
 mkdir -p "$OUTPUTS"
 
-ClangSharpPInvokeGenerator.cmd \
--f $BASEPATH/SDL/include/SDL3/SDL.h \
--f $BASEPATH/SDL/include/SDL3/SDL_stdinc.h \
--f $BASEPATH/SDL/include/SDL3/SDL_assert.h \
--f $BASEPATH/SDL/include/SDL3/SDL_asyncio.h \
--f $BASEPATH/SDL/include/SDL3/SDL_atomic.h \
--f $BASEPATH/SDL/include/SDL3/SDL_endian.h \
--f $BASEPATH/SDL/include/SDL3/SDL_error.h \
--f $BASEPATH/SDL/include/SDL3/SDL_properties.h \
--f $BASEPATH/SDL/include/SDL3/SDL_thread.h \
--f $BASEPATH/SDL/include/SDL3/SDL_mutex.h \
--f $BASEPATH/SDL/include/SDL3/SDL_iostream.h \
--f $BASEPATH/SDL/include/SDL3/SDL_audio.h \
--f $BASEPATH/SDL/include/SDL3/SDL_bits.h \
--f $BASEPATH/SDL/include/SDL3/SDL_blendmode.h \
--f $BASEPATH/SDL/include/SDL3/SDL_pixels.h \
--f $BASEPATH/SDL/include/SDL3/SDL_rect.h \
--f $BASEPATH/SDL/include/SDL3/SDL_surface.h \
--f $BASEPATH/SDL/include/SDL3/SDL_camera.h \
--f $BASEPATH/SDL/include/SDL3/SDL_clipboard.h \
--f $BASEPATH/SDL/include/SDL3/SDL_cpuinfo.h \
--f $BASEPATH/SDL/include/SDL3/SDL_video.h \
--f $BASEPATH/SDL/include/SDL3/SDL_dialog.h \
--f $BASEPATH/SDL/include/SDL3/SDL_guid.h \
--f $BASEPATH/SDL/include/SDL3/SDL_power.h \
--f $BASEPATH/SDL/include/SDL3/SDL_sensor.h \
--f $BASEPATH/SDL/include/SDL3/SDL_joystick.h \
--f $BASEPATH/SDL/include/SDL3/SDL_gamepad.h \
--f $BASEPATH/SDL/include/SDL3/SDL_scancode.h \
--f $BASEPATH/SDL/include/SDL3/SDL_keycode.h \
--f $BASEPATH/SDL/include/SDL3/SDL_keyboard.h \
--f $BASEPATH/SDL/include/SDL3/SDL_mouse.h \
--f $BASEPATH/SDL/include/SDL3/SDL_touch.h \
--f $BASEPATH/SDL/include/SDL3/SDL_pen.h \
--f $BASEPATH/SDL/include/SDL3/SDL_events.h \
--f $BASEPATH/SDL/include/SDL3/SDL_filesystem.h \
--f $BASEPATH/SDL/include/SDL3/SDL_gpu.h \
--f $BASEPATH/SDL/include/SDL3/SDL_haptic.h \
--f $BASEPATH/SDL/include/SDL3/SDL_hidapi.h \
--f $BASEPATH/SDL/include/SDL3/SDL_hints.h \
--f $BASEPATH/SDL/include/SDL3/SDL_init.h \
--f $BASEPATH/SDL/include/SDL3/SDL_loadso.h \
--f $BASEPATH/SDL/include/SDL3/SDL_locale.h \
--f $BASEPATH/SDL/include/SDL3/SDL_log.h \
--f $BASEPATH/SDL/include/SDL3/SDL_messagebox.h \
--f $BASEPATH/SDL/include/SDL3/SDL_metal.h \
--f $BASEPATH/SDL/include/SDL3/SDL_misc.h \
--f $BASEPATH/SDL/include/SDL3/SDL_platform.h \
--f $BASEPATH/SDL/include/SDL3/SDL_process.h \
--f $BASEPATH/SDL/include/SDL3/SDL_render.h \
--f $BASEPATH/SDL/include/SDL3/SDL_storage.h \
--f $BASEPATH/SDL/include/SDL3/SDL_system.h \
--f $BASEPATH/SDL/include/SDL3/SDL_time.h \
--f $BASEPATH/SDL/include/SDL3/SDL_timer.h \
--f $BASEPATH/SDL/include/SDL3/SDL_tray.h \
--f $BASEPATH/SDL/include/SDL3/SDL_version.h \
--f $BASEPATH/SDL/include/SDL3/SDL_main.h \
--I $BASEPATH/SDL/include \
--n Engine.SDL3 \
--m SDL \
--l SDL3 \
--c file=multi \
--o "$OUTPUTS" \
---with-access-specifier *=internal \
---generate funcs-with-body=false \
---generate helper-types
+# SDL
+if [ ! -d "$BASEPATH/SDL" ]; then
+    git clone --recurse-submodules https://github.com/libsdl-org/SDL.git "$BASEPATH/SDL"
+fi
 
-echo "Output files...."
-find "$OUTPUTS"
+# Files
+FILES=(
+  SDL_atomic
+  SDL_asyncio
+  SDL_audio
+  SDL_blendmode
+  SDL_camera
+  SDL_clipboard
+  SDL_cpuinfo
+  SDL_dialog
+  SDL_error
+  SDL_events
+  SDL_filesystem
+  SDL_gamepad
+  SDL_gpu
+  SDL_guid
+  SDL_haptic
+  SDL_hidapi
+  SDL_hints
+  SDL_init
+  SDL_iostream
+  SDL_joystick
+  SDL_keyboard
+  SDL_keycode
+  SDL_loadso
+  SDL_locale
+  SDL_log
+  SDL_main
+  SDL_messagebox
+  SDL_metal
+  SDL_misc
+  SDL_mouse
+  SDL_mutex
+  SDL_notification
+  SDL_pen
+  SDL_pixels
+  SDL_platform
+  SDL_power
+  SDL_process
+  SDL_properties
+  SDL_rect
+  SDL_render
+  SDL_revision
+  SDL_scancode
+  SDL_sensor
+  SDL_stdinc
+  SDL_storage
+  SDL_surface
+  SDL_thread
+  SDL_time
+  SDL_timer
+  SDL_tray
+  SDL_touch
+  SDL_version
+  SDL_video
+  SDL_vulkan
+)
+
+# Generate
+for file in "${FILES[@]}"; do
+  echo "Generating $file.h"
+
+  ARGS=(
+    -m SDL
+    -l SDL3
+    -n Engine.SDL3
+    -c file=single
+    
+    --remap char=byte
+    --remap Sint64=long
+    --remap Uint64=ulong
+    --remap bool=SDL_Bool
+
+    --generate macro-bindings
+    --generate fnptr-codegen=false
+    --generate funcs-with-body=false
+    
+    --exclude SDL_memcpy
+    --exclude SDL_memmove
+    --exclude SDL_memset
+    
+    --additional=-U_WIN32
+    --additional=-Ulinux
+    --additional=-U__linux
+    --additional=-U__linux__
+    --additional=-Uunix
+    --additional=-U__unix
+    --additional=-U__unix__
+    --additional=-U__APPLE__
+  )
+
+  ClangSharpPInvokeGenerator.cmd "${ARGS[@]}" -I "$BASEPATH/SDL/include" -f "$BASEPATH/SDL/include/SDL3/$file.h" -o "$OUTPUTS/$file.cs" || true
+  
+done
