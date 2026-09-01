57d1ee839d46d177ed06d810c3f167e16fbe981b
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-01 15:56:37 +0100

    SDL3

---
+++ Engine/Internals/SDL/SDL_Bool.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+namespace Engine.SDL3
+{
+    public readonly struct SDL_Bool
+    {
+        private readonly byte handle;
+
+        public SDL_Bool(bool value)
+        {
+            handle = (byte)(value ? 1 : 0);
+        }
+
+        public static implicit operator bool(SDL_Bool value) => value.handle != 0;
+        public static implicit operator SDL_Bool(bool value) => new SDL_Bool(value);
+    }
+}
---
+++ Engine/Internals/SDL/SDL_UTF8.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+namespace Engine.SDL3
+{
+    public static unsafe partial class SDL
+    {
+        public static string SDL_Utf8ToString(byte* ptr, bool free = false)
+        {
+            string text = Marshal.PtrToStringUTF8((IntPtr)ptr);
+        
+            if (free)
+            {
+                SDL.SDL_free(ptr);
+            }
+        
+            return text ?? "";
+        }
+    
+        public static byte[] SDL_StringToUtf8(string data)
+        {
+            if (data == null || data.Length <= 0)
+            {
+                return Array.Empty<byte>();
+            }
+        
+            return System.Text.Encoding.UTF8.GetBytes(data + '\0');
+        }
+    }
+}
