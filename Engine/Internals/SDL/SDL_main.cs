ef334633d855895b769c818ea8e417ec98aacf62
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-02 13:35:16 +0100

    Bindings

---
+++ Engine/Internals/SDL/SDL_main.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace Engine.SDL3
+{
+    public static unsafe partial class SDL
+    {
+        public static int SDL_main(int argc, byte** argv)
+        {
+            return iSDL_main(argc, argv);
+        }
+
+        public static void SDL_SetMainReady()
+        {
+            iSDL_SetMainReady();
+        }
+
+        public static int SDL_RunApp(int argc, byte** argv, IntPtr mainFunction, void* reserved)
+        {
+            return iSDL_RunApp(argc, argv, mainFunction, reserved);
+        }
+
+        public static int SDL_EnterAppMainCallbacks(int argc, byte** argv, IntPtr appinit, IntPtr appiter, IntPtr appevent, IntPtr appquit)
+        {
+            return iSDL_EnterAppMainCallbacks(argc, argv, appinit, appiter, appevent, appquit);
+        }
+
+        public static void SDL_GDKSuspendComplete()
+        {
+            iSDL_GDKSuspendComplete();
+        }
+    }
+}
---
+++ Engine/Internals/SDL/SDL_misc.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace Engine.SDL3
+{
+    public static unsafe partial class SDL
+    {
+        public static SDL_Bool SDL_OpenURL(byte* url)
+        {
+            return iSDL_OpenURL(url);
+        }
+    }
+}
