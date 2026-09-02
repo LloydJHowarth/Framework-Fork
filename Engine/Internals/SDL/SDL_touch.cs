77ea93ab3e6443dcb05622a04c2a81c156ad80c7
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-02 14:25:00 +0100

    Bindings

---
+++ Engine/Internals/SDL/SDL_touch.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace Engine.SDL3
+{
+    public static unsafe partial class SDL
+    {
+        public static ulong* SDL_GetTouchDevices(int* count)
+        {
+            return iSDL_GetTouchDevices(count);
+        }
+
+        public static byte* SDL_GetTouchDeviceName(ulong touchID)
+        {
+            return iSDL_GetTouchDeviceName(touchID);
+        }
+
+        public static SDL_TouchDeviceType SDL_GetTouchDeviceType(ulong touchID)
+        {
+            return iSDL_GetTouchDeviceType(touchID);
+        }
+
+        public static SDL_Finger** SDL_GetTouchFingers(ulong touchID, int* count)
+        {
+            return iSDL_GetTouchFingers(touchID, count);
+        }
+    }
+}
