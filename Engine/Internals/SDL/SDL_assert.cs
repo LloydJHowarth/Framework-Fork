6c0b565bd2944e0e54b5c5716e61be226fa2ee92
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-04 12:18:32 +0100

    Bindings

--- Engine/Internals/SDL/SDL_assert.cs
+++ Engine/Internals/SDL/SDL_assert.cs
@@ -5,9 +5,16 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static SDL_AssertState SDL_ReportAssertion(SDL_AssertData* data, byte* func, byte* file, int line)
+        public static SDL_AssertState SDL_ReportAssertion(SDL_AssertData* data, string func, string file, int line)
         {
-            return iSDL_ReportAssertion(data, func, file, line);
+            var value1 = SDL_StringToUtf8(func);
+            var value2 = SDL_StringToUtf8(file);
+            
+            fixed (byte* ptr1 = value1)
+            fixed (byte* ptr2 = value2)
+            {
+                return iSDL_ReportAssertion(data, ptr1, ptr2, line);
+            }
         }
 
         public static void SDL_SetAssertionHandler(IntPtr handler, void* userdata)
@@ -20,9 +27,12 @@ namespace Engine.SDL3
             return iSDL_GetDefaultAssertionHandler();
         }
 
-        public static IntPtr SDL_GetAssertionHandler(void** puserdata)
+        public static IntPtr SDL_GetAssertionHandler(out void* puserdata)
         {
-            return iSDL_GetAssertionHandler(puserdata);
+            fixed (void** userdataPtr = &puserdata)
+            {
+                return iSDL_GetAssertionHandler(userdataPtr);
+            }
         }
 
         public static SDL_AssertData* SDL_GetAssertionReport()
